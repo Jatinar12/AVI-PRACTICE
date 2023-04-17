f53bc22e16ec6e2dbf602f21afb8b98c21907573
Jatinar12 <127198129+Jatinar12@users.noreply.github.com>
2023-04-17 21:30:22 +0530

    Add files via upload

---
+++ 16 april/color/color.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+
+btn.addEventListener("click", ()=>{
+
+         btn.style.backgroundColor = "#008000" ;
+        
+})
+
+btn1.addEventListener("click", ()=>{
+
+    btn1.style.backgroundColor = "#FF0000" ;
+   
+})
+btn2.addEventListener("click", ()=>{
+
+    btn2.style.backgroundColor = "#008000" ;
+   
+})
+btn3.addEventListener("click", ()=>{
+
+    btn3.style.backgroundColor = "#FF0000" ;
+   
+})
