733fb92eb9d54b4125421919abc206c39b706098
Jatinar12 <127198129+Jatinar12@users.noreply.github.com>
2023-04-18 18:20:30 +0530

    Update ShallowComparison.js

--- 18 april/ShallowComparison.js
+++ 18 april/ShallowComparison.js
@@ -4,12 +4,12 @@ let obj = {
 }
 
 let obj2 = {
-    name: "Danish",
-    age: 24
+    name: "Jatin",
+    age: 23
 }
 
 let result= (obj === obj2) ? true : false;
 console.log(result)                                            //False
 
 let result1= (JSON.stringify(obj) === JSON.stringify(obj2)) ? true : false;
-console.log(result1)                                           //False
+console.log(result1)                                           //True
